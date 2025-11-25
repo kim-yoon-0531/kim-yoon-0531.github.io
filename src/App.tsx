657aa8140e88e0e7873583537d12f461fc32faa9
seul71 <yoonsk11@naver.com>
2025-11-25 11:22:13 +0900

    [추가] project init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+        <h1>Sangseon & Seulgi Are Getting Married</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            Like {count}
+          </button>
+        </div>
+        <div>
+          2026.05.31
+        </div>
+      </>
+  )
+}
+
+export default App

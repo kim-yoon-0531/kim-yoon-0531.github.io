d8820d7dcd3f054571a89456f95655cdacdc4b93
seul71 <yoonsk11@naver.com>
2026-04-08 22:17:32 +0900

    [수정] 사진수정 및 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import './App.css'
-import MockupPage from './components/MockupPage'
+import WeddingCard from './components/WeddingCard'
 
 function App() {
-  const params = new URLSearchParams(window.location.search)
-  const isMockup = params.get('v') === 'mockup'
-
-  if (isMockup) {
-    return <MockupPage />
-  }
-
-  return (
-    <div className="app landing">
-      <div className="landing-content">
-        <h1 className="landing-title">모바일청첩장 준비중입니다🤍🕊️</h1>
-      </div>
-    </div>
-  )
+  return <WeddingCard />
 }
 
 export default App
